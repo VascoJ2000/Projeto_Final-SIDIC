482e61a217ca1f12388df178af1ecadcf88e77ed
VascoJ2000 <vascojoao2000escola@gmail.com>
2024-04-20 16:19:50 +0100

    Cleanup on database controller + reorganizing some files

--- Shared/Abstract/base_controller.py
+++ Shared/Abstract/base_controller.py
@@ -1,11 +1,32 @@
 from abc import ABC, abstractmethod
 
 
-class BaseController(ABC):
+class DLBLController(ABC):
     def __init__(self):
         pass
 
     # User methods
+    @abstractmethod
+    def get_entry(self, collection, entry_id):
+        pass
+
+    @abstractmethod
+    def add_entry(self):
+        pass
+
+    @abstractmethod
+    def update_entry(self):
+        pass
+
+    @abstractmethod
+    def delete_entry(self):
+        pass
+
+
+class CLBLController(ABC):
+    def __init__(self):
+        pass
+
     @abstractmethod
     def get_user(self, user_id, email):
         pass
@@ -20,4 +41,12 @@ class BaseController(ABC):
 
     @abstractmethod
     def delete_user(self):
+        pass
+
+    @abstractmethod
+    def login(self, email, password):
+        pass
+
+    @abstractmethod
+    def token(self):
         pass
