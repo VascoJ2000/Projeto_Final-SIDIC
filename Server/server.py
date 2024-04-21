659dc64a1a74afc29bd4d73c8db5d1496339fce2
VascoJ2000 <vascojoao2000escola@gmail.com>
2024-04-21 17:14:16 +0100

    minor changes

--- Server/server.py
+++ Server/server.py
@@ -12,11 +12,8 @@ class BusinessLayerServer(Server):
         self.run_server()
 
     def setup_routes(self):
-        # Common Routes
-        self.app.route('/<coll>&<entry_id>', methods=['GET'])(self.controller.get_entry)
-        self.app.route('/all/<coll>', methods=['GET'])(self.controller.get_all_entries)
-        self.app.route('/', methods=['POST'])(self.controller.add_entry)
-        self.app.route('/', methods=['PUT'])(self.controller.update_entry)
-        self.app.route('/<coll>&<entry_id>', methods=['DELETE'])(self.controller.delete_entry)
+        # User Layer to Business Layer methods
+        pass
 
-data_layer = DataLayerServer()
+
+data_layer = BusinessLayerServer()
