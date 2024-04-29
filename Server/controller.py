c5ec14afb029cd1005e8db529ab1b46b1163731b
VascoJ2000 <vascojoao2000escola@gmail.com>
2024-04-29 20:17:45 +0100

    Added password hashing and jwt (not fully implememented nor tested)

--- Server/controller.py
+++ Server/controller.py
@@ -2,7 +2,15 @@ from Shared.Abstract import CLBLLinker
 from Server.client import BLClient
 from Server.middleware import auth
 from flask import request, Response, g
+from dotenv import load_dotenv
+from argon2 import PasswordHasher
+import datetime
 import json
+import jwt
+import os
+
+
+load_dotenv()
 
 
 class Controller(CLBLLinker):
@@ -11,15 +19,19 @@ class Controller(CLBLLinker):
         self.cli = BLClient()
 
     # User methods
+    @auth(key=os.getenv('ACCESS_KEY'))
     def get_user(self, user_id, email):
         pass
 
+    @auth(key=os.getenv('ACCESS_KEY'))
     def add_user(self):
         pass
 
+    @auth(key=os.getenv('ACCESS_KEY'))
     def update_user(self):
         pass
 
+    @auth(key=os.getenv('ACCESS_KEY'))
     def delete_user(self):
         pass
 
@@ -42,3 +54,44 @@ class Controller(CLBLLinker):
 
     def token(self):
         pass
+
+
+def password_hash(password):
+    ph = PasswordHasher()
+    hashed_password = ph.hash(password)
+    return hashed_password
+
+
+def password_verify(input_password, hashed_password):
+    ph = PasswordHasher()
+    if ph.verify(input_password, hashed_password):
+        return True
+    raise Exception('Password is invalid!')
+
+
+def generate_token(user_data):
+    payload = {
+        'User_id': user_data['User_id'],
+        'Email': user_data['Email'],
+        'Username': user_data['Username'],
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1)
+    }
+    jwt_token = jwt.encode(payload, os.getenv('ACCESS_KEY'), algorithm='HS256')
+    return jwt_token
+
+
+def verify_token(header):
+    try:
+        if 'Authorization' in header:
+            auth_header = header.get('Authorization')
+            token = auth_header.split(' ')[1]
+        else:
+            raise Exception('No authorization header')
+        token_decoded = jwt.decode(token, os.getenv('ACCESS_KEY'), algorithms='HS256')
+    except jwt.ExpiredSignatureError:
+        raise Exception("Token has expired")
+    except jwt.InvalidTokenError as e:
+        raise Exception("Token is Invalid: " + str(e))
+    else:
+        return token_decoded
+
