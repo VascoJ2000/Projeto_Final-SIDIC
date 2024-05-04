f4761878289391a5411ed6d1bdd5b519bb3950d1
VascoJ2000 <vascojoao2000escola@gmail.com>
2024-05-04 18:03:07 +0100

    Authentication middleware complete

--- ChatFlow/src/user.py
+++ ChatFlow/src/user.py
@@ -1,21 +1,28 @@
 from __main__ import app
+from ChatFlow.middleware.auth import auth_access
+from flask import Response, request, g
 
 
 @app.route('/user', methods=['GET'])
-def get_user(user_id, email):
-    pass
+@auth_access
+def get_user():
+    email = g.decoded_jwt['email']
+    return Response(email, status=200)
 
 
 @app.route('/user', methods=['POST'])
+@auth_access
 def add_user():
     pass
 
 
 @app.route('/user', methods=['PUT'])
+@auth_access
 def update_user():
     pass
 
 
 @app.route('/user', methods=['DELETE'])
+@auth_access
 def delete_user():
     pass
