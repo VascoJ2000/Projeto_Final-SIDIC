4da1e7394547d87bfc20e434d121e1fda7ecb412
VascoJ2000 <vascojoao2000escola@gmail.com>
2024-05-04 18:03:07 +0100

    Authentication middleware complete

---
+++ ChatFlow/middleware/auth.py
@@ -0,0 +1,29 @@
+from functools import wraps
+from flask import request, Response, g
+import jwt
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+def auth_access(func):
+    @wraps(func)
+    def _auth_access(*args, **kwargs):
+        if request.cookies.get('chatflow-access_token'):
+            g.decoded_jwt = verify_token(request.cookies.get('chatflow-access_token'))
+            return func(*args, **kwargs)
+
+        return Response('No token', status=401)
+
+    return _auth_access
+
+
+def verify_token(token):
+    try:
+        token_decoded = jwt.decode(token, os.getenv('ACCESS_KEY'), algorithms=['HS256'])
+    except jwt.ExpiredSignatureError:
+        return Response("Token has expired", status=403)
+    except jwt.InvalidTokenError as e:
+        return Response("Token is Invalid: " + str(e), status=403)
+    return token_decoded
